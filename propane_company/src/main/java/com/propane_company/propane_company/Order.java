f1be8f237bc79010298f002290a6cdd6f6f86f30
utshav khatiwada <utshavkhatiwada4@gmail.com>
2024-07-07 09:09:19 -0400

    Added JavaDocs comments to improve readability of the code

--- propane_company/src/main/java/com/propane_company/propane_company/Order.java
+++ propane_company/src/main/java/com/propane_company/propane_company/Order.java
@@ -32,43 +32,82 @@ public class Order {
      * @return
      */
 
+    /**
+     * Sets the quantity of the order.
+     * @param quantity the quantity of the order
+     */
     public void setQuantity(Integer quantity) {
         this.quantity = quantity;
     }
 
+    /**
+     * Returns the quantity of the order.
+     * @return the quantity of the order
+     */
     public Integer getQuantity() {
         return quantity;
     }
 
+    /**
+     * Returns the order ID.
+     * @return the order ID
+     */
     public Integer getOrderId(){
         return orderId;
     }
 
+    /**
+     * Sets the order ID.
+     * @param orderId the order ID
+     */
     public void setOrderId(Integer orderId){
         this.orderId = orderId;
     }
 
-    // Getters and setters
+    /**
+     * Returns the list of propane tanks associated with the order.
+     * @return the list of propane tanks
+     */
     public List<PropaneTank> getPropaneTanks() {
         return propaneTanks;
     }
 
+    /**
+     * Sets the list of propane tanks associated with the order.
+     * @param propaneTanks the list of propane tanks
+     */
     public void setPropaneTanks(List<PropaneTank> propaneTanks) {
         this.propaneTanks = propaneTanks;
     }
 
+    /**
+     * Returns the customer ID.
+     * @return the customer ID
+     */
     public Integer getCustomerId(){
         return customerId;
     }
 
+    /**
+     * Sets the customer ID.
+     * @param customerId the customer ID
+     */
     public void setCustomerId(Integer customerId){
         this.customerId = customerId;
     }
 
-   public Date getOrderDate(){
+    /**
+     * Returns the order date.
+     * @return the order date
+     */
+    public Date getOrderDate(){
         return orderDate;
     }
 
+    /**
+     * Sets the order date.
+     * @param orderDate the order date
+     */
     public void setOrderDate(Date orderDate){
         this.orderDate = orderDate;
     }
